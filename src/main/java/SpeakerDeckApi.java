59a36e4a23aff54b0de17b7d48ccc1591a739c2b
Ivo <irbatistela@gmail.com>
2021-06-19 22:04:41 -0300

    fetch display name for given username

--- src/main/java/SpeakerDeckApi.java
+++ src/main/java/SpeakerDeckApi.java
@@ -1,9 +1,49 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class SpeakerDeckApi {
     public boolean isWorking() {
         return true;
     }
 
     public SpeakerProfile fetch(String username) {
-        return new SpeakerProfile("Wagner Voltz - Fusca", 8);
+        String displayName = fetchDisplayName(username);
+
+        return new SpeakerProfile(displayName, 8);
+    }
+
+    private String fetchDisplayName(String username) {
+        try {
+            String body = getHTMLBodyContentFromUsernameProfile(username);
+
+            String pageTitle = getTitleTagValueFromBody(body);
+            return extractDisplayNameFromTitleTagValue(pageTitle);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Not Found";
+    }
+
+    private String getHTMLBodyContentFromUsernameProfile(String username) throws java.io.IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create("https://speakerdeck.com/" + username))
+                .build();
+
+        return httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString()).body();
+    }
+
+    private String getTitleTagValueFromBody(String body) {
+        int start = body.indexOf("<title>") + "<title>".length();
+        int end = body.indexOf("</title>", start);
+
+       return body.substring(start, end);
+    }
+
+    private String extractDisplayNameFromTitleTagValue(String pageTitle) {
+        // The tile tag contains: Wagner Voltz - Fusca (@wagnerfusca) on Speaker Deck
+        return pageTitle.substring(0, pageTitle.indexOf('(')).trim();
     }
 }
